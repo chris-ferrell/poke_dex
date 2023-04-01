ac926f7226e93f070bc19be8cf78d85925a83b4f
unknown-bytes <unknown.develops@gmail.com>
2023-04-01 08:32:14 -0400

    redirecting routes to controllers

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+//router setup
+const router = express.Router();
+// importing the poke_router
+const poke_router = require('./controller/poke_router');
+
+//middle ware
 
 
-
+// Setup routes
+app.use('/', pokemonRouter)
 
 app.listen(PORT, () => {
     console.log('listening...')
