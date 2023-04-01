f9f1546c9626a34d2b2ba3747a6ccc3f902d427f
unknown-bytes <unknown.develops@gmail.com>
2023-04-01 08:34:16 -0400

    setting up controller

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const PORT = 3000;
 //router setup
 const router = express.Router();
 // importing the poke_router
-const poke_router = require('./controller/poke_router');
+const poke_router = require('./controllers/poke_router');
 
 //middle ware
-
-
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true}));
 // Setup routes
 app.use('/', pokemonRouter)
 
