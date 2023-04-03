36d7d02aa3634895d339f1174b0d4c8762e7acea
unknown-bytes <unknown.develops@gmail.com>
2023-04-02 22:27:36 -0400

    fixes

--- controllers/poke_router.js
+++ controllers/poke_router.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const router  = express();
-const pokemons = require("../models/testData")
+const pokemons = require("../models/testData");
+const pokemon = require('../models/pokemon');
 // middleware
 router.set('view engine', 'ejs');
 
@@ -11,6 +12,13 @@ router.use(methodOverride('_method'));
 router.get('/pokemon', (req, res) => {
     res.render('pokemon_index', { pokemons });
 });
+
+
+// Route for displaying the form for creating a new pokemon
+router.get('/pokemon/new', (req, res) => {
+  res.render('pokemon_new');
+});
+
 
 // showing the pokemon at index 
 router.get('/pokemon/:id', (req, res) => {
@@ -37,11 +45,7 @@ router.get('/pokemon/:id', (req, res) => {
     
   });
   
-// Route for displaying the form for creating a new pokemon
-router.get('/pokemon/new', (req, res) => {
-    res.render('pokemon_new');
-  });
-  
+
   // Route for displaying the form for editing an existing pokemon
   router.get('/pokemon/:id/edit', (req, res) => {
     const pokemon = pokemons.find(pokemon => pokemon.id == req.params.id);
@@ -53,7 +57,7 @@ router.post('/pokemon', (req, res) => {
     const test = pokemons[pokemons.length] + 1
     console.log(pokemons[pokemons.length-1].id+=1)
     const { name, type, level, moves } = req.body;
-    const newPokemon = { id: test, name, type, level, moves };
+    const newPokemon = { id: Number(pokemons.length + 1), name, type, level, moves };
     pokemons.push(newPokemon);
     //                       does not work!!!
     res.redirect(`/pokemon/${newPokemon.id}`);
